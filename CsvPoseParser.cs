0c66dea1e15a676fc56bd57fabc082d33ddafb00
Firas Assaad <assaad.firas@gmail.com>
2024-06-17 12:49:29 +0200

    CSV Rectangular Frame Format
    - Allow specifying frames across multiple rows/columns when pasting CSV poses.

--- CsvPoseParser.cs
+++ CsvPoseParser.cs
@@ -1,6 +1,7 @@
 ﻿using SpriteEditor.Models;
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace SpriteEditor
 {
@@ -36,6 +37,55 @@ namespace SpriteEditor
             // Name, Destination X, Destination Y, Width, Height, Source X, Source Y, Frame Count
             { 8, new LineFormat(0, 3, 5, 7) },
         };
+
+        private static void AddFrames(int x, int y, int width, int height,
+                int count, bool isVertical, bool isRectangular, int perRow,
+                List<Frame> frames)
+        {
+            var originalX = x;
+            var originalY = y;
+
+            for (var i = 0; i < count - 1; i++)
+            {
+                var nextRowOrColumn = isRectangular && (i + 1) % perRow == 0;
+                if (isVertical)
+                {
+                    y += height;
+                    if (nextRowOrColumn)
+                    {
+                        x += width;
+                        y = originalY;
+                    }
+                }
+                else
+                {
+                    x += width;
+                    if (nextRowOrColumn)
+                    {
+                        y += height;
+                        x = originalX;
+                    }
+                }
+
+                frames.Add(
+                    new Frame
+                    {
+                        Rectangle = new Rect(x, y, width, height)
+                    }
+                );
+            }
+        }
+
+        private const string framePattern = @"(?<frameCount>\d+)(?<direction>V|H)?(?<perRow>\d+)?";
+        private static readonly Regex frameRegex = new(framePattern,
+            RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        private static bool CheckFramePatternMatch(Group frameCount, Group direction, Group perRow)
+        {
+            return frameCount.Success
+                && (!perRow.Success || direction.Success);
+        }
+
         private static (Pose pose, string error) ParseLine(string line)
         {
             var parts = line.Split(',', SplitOptions);
@@ -75,36 +125,45 @@ namespace SpriteEditor
             if (format.FrameCountIndex.HasValue)
             {
                 // 3V means 3 vertical frames, 3H or 3 means horizontal frames
+                // 4V2 means 4 vertical frames with 2 frames per row
+                // 4H2 means 4 horizontal frames with 2 frames per column
                 var countString = parts[format.FrameCountIndex.Value].ToUpper();
-                var isVertical = countString.EndsWith("V");
-                if (isVertical || countString.EndsWith("H"))
+                var match = frameRegex.Match(countString);
+                if (!match.Success)
                 {
-                    countString = countString.Substring(0, countString.Length - 1);
+                    return (null, $"Error: Invalid frame count format in line: {line}");
                 }
 
-                if (!int.TryParse(countString, out int count) || count < 2)
+                var frameCountGroup = match.Groups["frameCount"];
+                var directionGroup = match.Groups["direction"];
+                var perRowGroup = match.Groups["perRow"];
+
+                if (!CheckFramePatternMatch(frameCountGroup, directionGroup, perRowGroup))
                 {
                     return (null, $"Error: Invalid frame count in line: {line}");
                 }
 
-                for (var i = 0; i < count - 1; i++)
-                {
-                    if (isVertical)
-                    {
-                        y += height;
-                    }
-                    else
-                    {
-                        x += width;
-                    }
+                var isVertical = directionGroup.Success
+                    && directionGroup.Value.ToUpper() == "V";
 
-                    frames.Add(
-                        new Frame
-                        {
-                            Rectangle = new Rect(x, y, width, height)
-                        }
-                    );
+
+                if (!int.TryParse(frameCountGroup.Value, out int count) || count < 2)
+                {
+                    return (null, $"Error: Invalid frame count in line: {line}");
                 }
+
+                var perRow = count;
+                var isRectangular = false;
+                if (perRowGroup.Success)
+                {
+                    if (!int.TryParse(perRowGroup.Value, out perRow) || perRow < 2)
+                    {
+                        return (null, $"Error: Invalid per row/column in line: {line}");
+                    }
+                    isRectangular = true;
+                }
+
+                AddFrames(x, y, width, height, count, isVertical, isRectangular, perRow, frames);
             }
 
             var pose = new Pose
